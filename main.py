bb4ecb513d9a0d556827ca09bdf00aefd5a4aed8
jerryold <jerry86064@gmail.com>
2024-07-08 10:02:51 +0800

    Linebot_Gemini

---
+++ main.py
@@ -0,0 +1,119 @@
+from linebot.models import FlexSendMessage
+from linebot.models import (
+    MessageEvent, TextSendMessage
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.aiohttp_async_http_client import AiohttpAsyncHttpClient
+from linebot import (
+    AsyncLineBotApi, WebhookParser
+)
+from fastapi import Request, FastAPI, HTTPException
+import google.generativeai as genai
+import os
+import sys
+from io import BytesIO
+
+import aiohttp
+import PIL.Image
+
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('ChannelSecret', None)
+channel_access_token = os.getenv('ChannelAccessToken', None)
+gemini_key = os.getenv('GEMINI_API_KEY') 
+
+# channel_secret="8d99523725baf0e11b2971b93d363449"
+# channel_access_token="wOj/Mx4oRX7IU4RJ00l6IXqeFQ6EKe3cTVR95iXiAwDKOyW5LBsxladVAfomOV39FRBdV7nwaw2fLq32SYp0CJ56BQwpSorze0Bxmcm9I08mdHhFXzZsiAlqXtMrlYSy6BpNDEXDBfW5DVSNYW1UogdB04t89/1O/w1cDnyilFU="
+# gemini_key = "AIzaSyCQnjvsYaFa2GvPk8QvHgg9CRBMdaGDIOk"
+imgage_prompt = '''
+Describe this image with scientific detail, reply in zh-TW:
+'''
+
+if channel_secret is None:
+    print('Specify ChannelSecret as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify ChannelAccessToken as environment variable.')
+    sys.exit(1)
+if gemini_key is None:
+    print('Specify GEMINI_API_KEY as environment variable.')
+    sys.exit(1)
+
+# Initialize the FastAPI app for LINEBot
+app = FastAPI()
+session = aiohttp.ClientSession()
+async_http_client = AiohttpAsyncHttpClient(session)
+line_bot_api = AsyncLineBotApi(channel_access_token, async_http_client)
+parser = WebhookParser(channel_secret)
+
+# Initialize the Gemini Pro API
+genai.configure(api_key=gemini_key)
+
+
+@app.post("/")
+async def handle_callback(request: Request):
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = await request.body()
+    body = body.decode()
+
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Invalid signature")
+
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+
+        if (event.message.type == "text"):
+            # Provide a default value for reply_msg
+            msg = event.message.text
+            ret = generate_gemini_text_complete(f'{msg}, reply in zh-TW:')
+            reply_msg = TextSendMessage(text=ret.text)
+            await line_bot_api.reply_message(
+                event.reply_token,
+                reply_msg
+            )
+        elif (event.message.type == "image"):
+            message_content = await line_bot_api.get_message_content(
+                event.message.id)
+            image_content = b''
+            async for s in message_content.iter_content():
+                image_content += s
+            img = PIL.Image.open(BytesIO(image_content))
+
+            result = generate_result_from_image(img, imgage_prompt)
+            reply_msg = TextSendMessage(text=result.text)
+            await line_bot_api.reply_message(
+                event.reply_token,
+                reply_msg
+            )
+            return 'OK'
+        else:
+            continue
+
+    return 'OK'
+
+
+def generate_gemini_text_complete(prompt):
+    """
+    Generate a text completion using the generative model.
+    """
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(prompt)
+    return response
+
+
+def generate_result_from_image(img, prompt):
+    """
+    Generate a image vision result using the generative model.
+    """
+
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([prompt, img], stream=True)
+    response.resolve()
+    return response
